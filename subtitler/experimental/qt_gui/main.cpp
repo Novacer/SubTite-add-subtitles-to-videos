b7288bca09f1521d6eb3444e3c8488346b4ec2a3
Novacer <zhangjack27@gmail.com>
2022-02-21 19:56:16 -0500

    remove debug logging where necessary

--- subtitler/experimental/qt_gui/main.cpp
+++ subtitler/experimental/qt_gui/main.cpp
@@ -1,3 +1,9 @@
+#define __STDC_CONSTANT_MACROS
+
+extern "C" {
+#include <libavutil/log.h>
+}
+
 #include <QApplication>
 #include <QDir>
 #include <QFile>
@@ -5,6 +11,8 @@
 #include "subtitler/experimental/qt_gui/player_window.h"
 
 int main(int argc, char *argv[]) {
+    av_log_set_level(AV_LOG_FATAL);
+
     QApplication app(argc, argv);
 
     Q_INIT_RESOURCE(timeline);

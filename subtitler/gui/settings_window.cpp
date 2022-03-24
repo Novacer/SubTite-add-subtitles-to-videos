999c96f4d730d264098bb33169f8d449b06b2877
Jack <zhangjack27@gmail.com>
2022-03-24 16:08:54 -0700

    Add settings dialog (#77)
    * add settings dialog
    * adjust font

---
+++ subtitler/gui/settings_window.cpp
@@ -0,0 +1,65 @@
+#include "subtitler/gui/settings_window.h"
+
+#include <QDebug>
+#include <QFileDialog>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+
+namespace subtitler {
+namespace gui {
+
+Settings GetSettings(const Settings &current_settings) {
+    Settings new_settings = current_settings;
+    SettingsWindow setting_dialog{new_settings};
+    setting_dialog.exec();
+    return new_settings;
+}
+
+SettingsWindow::SettingsWindow(Settings &settings, QWidget *parent)
+    : QDialog{parent}, settings_{settings} {
+    setWindowTitle("Choose Files");
+
+    QPushButton *choose_video = new QPushButton{tr("Choose Video File"), this};
+    QPushButton *choose_subtitle =
+        new QPushButton{tr("Choose Subtitle File"), this};
+    QPushButton *close = new QPushButton{tr("Done"), this};
+
+    QLabel *video_choice = new QLabel{this};
+    video_choice->setMinimumWidth(300);
+    QLabel *subtitle_choice = new QLabel{this};
+    subtitle_choice->setMinimumWidth(300);
+
+    QGridLayout *layout = new QGridLayout{this};
+    layout->addWidget(choose_video, 0, 0);
+    layout->addWidget(video_choice, 0, 1);
+    layout->addWidget(choose_subtitle, 1, 0);
+    layout->addWidget(subtitle_choice, 1, 1);
+    layout->addWidget(close, 2, 1, Qt::AlignRight);
+
+    layout->setVerticalSpacing(5);
+
+    connect(choose_video, &QPushButton::clicked, this, [this, video_choice]() {
+        settings_.video_file = QFileDialog::getOpenFileName(
+            /* parent= */ this,
+            /* caption= */ tr("Open Video"),
+            /* directory= */ "",
+            /* filter= */ tr("Video Files (*.mp4)"));
+        video_choice->setText(settings_.video_file);
+    });
+
+    connect(choose_subtitle, &QPushButton::clicked, this,
+            [this, subtitle_choice]() {
+                settings_.subtitle_file = QFileDialog::getSaveFileName(
+                    /* parent= */ this,
+                    /* caption= */ tr("Create/Open Subtitle File"),
+                    /* directory= */ "",
+                    /* filter= */ tr("SRT Files (*.srt)"));
+                subtitle_choice->setText(settings_.subtitle_file);
+            });
+
+    connect(close, &QPushButton::clicked, this, [this]() { this->done(0); });
+}
+
+}  // namespace gui
+}  // namespace subtitler
---
+++ subtitler/gui/settings_window.h
@@ -0,0 +1,30 @@
+#ifndef SUBTITLER_GUI_SETTINGS_WINDOW_H
+#define SUBTITLER_GUI_SETTINGS_WINDOW_H
+
+#include <QDialog>
+#include <QString>
+
+namespace subtitler {
+namespace gui {
+
+struct Settings {
+    QString video_file;
+    QString subtitle_file;
+};
+
+Settings GetSettings(const Settings &current_settings);
+
+class SettingsWindow : public QDialog {
+    Q_OBJECT
+  public:
+    SettingsWindow(Settings &settings, QWidget *parent = Q_NULLPTR);
+    ~SettingsWindow() = default;
+
+  private:
+    Settings &settings_;
+};
+
+}  // namespace gui
+}  // namespace subtitler
+
+#endif

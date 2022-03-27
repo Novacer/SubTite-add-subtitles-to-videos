f29d9c60351b81f126d3c4e8cb298f1ccbcd1a99
Novacer <zhangjack27@gmail.com>
2022-03-27 15:24:44 -0400

    add docs

--- subtitler/video/processing/progress_parser.h
+++ subtitler/video/processing/progress_parser.h
@@ -9,6 +9,9 @@ namespace subtitler {
 namespace video {
 namespace processing {
 
+/**
+ * Struct containing progress info while running FFMPEG.
+ */
 struct Progress {
     uint32_t frame = 0;
     double fps = 0.0;
@@ -22,11 +25,27 @@ struct Progress {
     std::string progress;
 };
 
+/**
+ * Parses FFMPEG progress and returns as a Progress struct.
+ * Data from FFMPEG can be received in chunks. If an incomplete progress
+ * update is received, then this parser will buffer the data until
+ * a complete update can be returned.
+ *
+ * If multiple updates are received at once, then this returns the most
+ * recent update.
+ */
 class ProgressParser {
   public:
     ProgressParser() = default;
     ~ProgressParser() = default;
 
+    /**
+     * Receive progress data from FFMPEG and return the most recent update.
+     *
+     * @param input the progress data from FFMPEG.
+     * @return std::optional<Progress> Will be nullopt if not enough data
+     * received to give an update. Otherwise, returns the most recent update.
+     */
     std::optional<Progress> Receive(const char* input);
 
   private:

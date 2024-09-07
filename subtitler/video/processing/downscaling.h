953787b0c02df2f592e69df0caadd284881a5a59
Jack Attack <zhangjack27@gmail.com>
2024-09-06 23:02:45 -0700

    Add a helper for getting optimal downscale settings (#120)
    * Add a helper for getting optimal downscale settings
    
    * update docs

---
+++ subtitler/video/processing/downscaling.h
@@ -0,0 +1,35 @@
+#ifndef SUBTITLER_VIDEO_PROCESSING_DOWNSCALING_H
+#define SUBTITLER_VIDEO_PROCESSING_DOWNSCALING_H
+
+#include <string>
+
+/**
+ * Implements methods for downscaling videos above 1080p@60fps.
+ * In the GUI, such videos tend to be more latent when presented through
+ * the OpenGL renderer. As a workaround, we can downscale them via
+ * FFMPEG. Roughly, this method allows us to play videos up to ~4k@60fps without
+ * much issue.
+ *
+ */
+
+namespace subtitler {
+namespace video {
+namespace processing {
+
+// Information about the input video.
+struct InputVideoScalingInfo {
+    int height;
+    int width;
+    double fps;
+};
+
+// Given a video's dimensions and fps, this returns the recommended filters to
+// pass to libav for downscaling.
+std::string GetFFMpegScaleFilterRecommendation(
+    const InputVideoScalingInfo input);
+
+}  // namespace processing
+}  // namespace video
+}  // namespace subtitler
+
+#endif
---
+++ subtitler/video/processing/downscaling_test.cpp
@@ -0,0 +1,64 @@
+#include "subtitler/video/processing/downscaling.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+namespace subtitler {
+namespace video {
+namespace processing {
+
+namespace {
+
+constexpr std::string_view SCALE_FILTER = "scale=-1:720:flags=lanczos";
+constexpr std::string_view FPS_SCALE_FILTER =
+    "fps=30,scale=-1:720:flags=lanczos";
+
+TEST(DownscalingTest, Video_720p_30fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/1280, /*height=*/720, /*fps=*/30
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), "");
+}
+
+TEST(DownscalingTest, Video_720p_60fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/1280, /*height=*/720, /*fps=*/60
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), "");
+}
+
+TEST(DownscalingTest, Video_1080p_30fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/1920, /*height=*/1080, /*fps=*/30
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), SCALE_FILTER);
+}
+
+TEST(DownscalingTest, Video_1080p_60fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/1920, /*height=*/1080, /*fps=*/60
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), FPS_SCALE_FILTER);
+}
+
+TEST(DownscalingTest, Video_4K_30fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/3840, /*height=*/2160, /*fps=*/30
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), SCALE_FILTER);
+}
+
+TEST(DownscalingTest, Video_4K_60fps) {
+    struct InputVideoScalingInfo input {
+        /*width=*/3840, /*height=*/2160, /*fps=*/60
+    };
+    EXPECT_EQ(GetFFMpegScaleFilterRecommendation(input), FPS_SCALE_FILTER);
+}
+
+}  // namespace
+
+}  // namespace processing
+}  // namespace video
+}  // namespace subtitler
